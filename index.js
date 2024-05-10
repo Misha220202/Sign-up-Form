668db6133e58f8082a14b38e33d193235d63919b
Misha220202 <157743520+Misha220202@users.noreply.github.com>
2024-05-10 16:17:45 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+function toUpperCase(event) {
+    let value = event.target.value;
+    value = value.toUpperCase();
+    event.target.value = value;
+}
+
+document.querySelector('#first_name').addEventListener('input', toUpperCase);
+document.querySelector('#last_name').addEventListener('input', toUpperCase);
+
+document.querySelector('#phone_number').addEventListener('input', event => {
+    let value = event.target.value;
+    value = value.replace(/-/g, '');
+    if (value.length > 3 && value.length <= 6) {
+        value = value.slice(0, 3) + '-' + value.slice(3);
+    } else if (value.length > 6 && value.length <= 10) {
+        value = value.slice(0, 3) + '-' + value.slice(3, 6) + '-' + value.slice(6);
+    }
+    event.target.value = value;
+});
+
+const password = document.querySelector('#password');
+const confirm_password = document.querySelector('#confirm_password');
+
+function checkPassword() {
+    const form = document.querySelector('form');
+    let password1 = password.value;
+    let password2 = confirm_password.value;
+    if (password1 !== password2) {
+        password.classList.add('password-mismatch');
+        confirm_password.classList.add('password-mismatch');
+        if (form.lastElementChild.tagName.toLowerCase() === 'fieldset') {
+            const note = document.createElement('p');
+            note.textContent = '*Passwords do not match';
+            note.style.color = 'red';
+            note.style.fontSize = '0.7rem';
+            form.appendChild(note);
+        }
+    } else {
+        password.classList.remove('password-mismatch');
+        confirm_password.classList.remove('password-mismatch');
+        if (form.lastElementChild.tagName.toLowerCase() === 'p') {
+            form.removeChild(form.lastElementChild);
+        }
+    }
+}
+
+password.addEventListener('input', checkPassword);
+confirm_password.addEventListener('input', checkPassword);
+
+document.querySelector('button').addEventListener('mouseenter', event => button.focus());
+document.querySelector('button').addEventListener('click', event => location.reload());
